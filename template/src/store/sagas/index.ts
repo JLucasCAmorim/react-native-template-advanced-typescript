b0f8f8c2d72f0f2f49111059e0e33184a1873f1a
João Lucas Evangelista <jlucasevangelista153@gmail.com>
2020-05-09 11:55:22 -0300

    Updating Package JSON and React Code

--- template/src/store/sagas/index.ts
+++ template/src/store/sagas/index.ts
@@ -1,5 +1,9 @@
-import {all} from 'redux-saga/effects';
+import {all, AllEffect} from 'redux-saga/effects';
 
-export default function* rootSaga() {
+export default function* rootSaga(): Generator<
+  AllEffect<never>,
+  void,
+  unknown
+> {
   yield all([]);
 }
